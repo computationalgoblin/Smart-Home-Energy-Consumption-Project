91d1cee8b4a2b08a77dfb36a8a9acb669b1deed0
Ángel Expósito García <angelexpgar@gmail.com>
2024-04-29 20:56:02 +0200

    update eda

--- utils/eda_utils.py
+++ utils/eda_utils.py
@@ -16,3 +16,31 @@ def plot_dist_subplots(data, columns):
     
     plt.tight_layout()
     plt.show()
+
+
+def plot_subplots(data, cols: list, freq: str = "D"):
+    """
+    Args:
+        data: the df containing the data.
+        cols: a list of column names to plot.
+        freq: the resampling frequence
+    """
+    # Resample the data at a daily frequency and calculate the mean for each day
+    resampled_data = data[cols].resample(freq).mean()
+    
+    # Plot the resampled data in subplots
+    resampled_data.plot(subplots=True, layout=(-1, 2), figsize=(20, 15), grid=True)
+
+
+def time_series_subplots(data, cols):
+    for freq in ["D", "W", "M"]:
+        if freq == "D":
+            title = "DAILY"
+        elif freq == "W":
+            title = "WEEKLY"
+        elif freq == "M":
+            title = "MONTHLY"
+        
+        print(f"---------------------- {title} ----------------------------")
+        plot_subplots(data, cols, freq=freq)
+        plt.show()
